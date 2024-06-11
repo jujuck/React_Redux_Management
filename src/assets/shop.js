827e7163eba5ebd340e6aadb94f6b150cdb11d5c
Julien RICHARD <julien.y.richard@gmail.com>
2024-06-11 11:35:34 +0200

    Mise en import du state initial

---
+++ src/assets/shop.js
@@ -0,0 +1,68 @@
+const shoppingList = [
+  {
+    id: 1,
+    product: "Lait",
+    brand: "Lactel",
+    quantity: 2,
+    store: "Carrefour",
+    status: "to buy",
+  },
+  {
+    id: 2,
+    product: "Pain",
+    brand: "Boulangerie Paul",
+    quantity: 3,
+    store: "Auchan",
+    status: "done",
+  },
+  {
+    id: 3,
+    product: "Pâtes",
+    brand: "Barilla",
+    quantity: 5,
+    store: "Leclerc",
+    status: "to buy",
+  },
+  {
+    id: 4,
+    product: "Yaourt",
+    brand: "Yoplait",
+    quantity: 6,
+    store: "Carrefour",
+    status: "cancelled",
+  },
+  {
+    id: 5,
+    product: "Beurre",
+    brand: "Président",
+    quantity: 1,
+    store: "Auchan",
+    status: "done",
+  },
+  {
+    id: 6,
+    product: "Eau",
+    brand: "Evian",
+    quantity: 4,
+    store: "Leclerc",
+    status: "to buy",
+  },
+  {
+    id: 7,
+    product: "Jus d'orange",
+    brand: "Tropicana",
+    quantity: 2,
+    store: "Carrefour",
+    status: "done",
+  },
+  {
+    id: 8,
+    product: "Fromage",
+    brand: "Babybel",
+    quantity: 10,
+    store: "Auchan",
+    status: "to buy",
+  },
+];
+
+export default shoppingList;
