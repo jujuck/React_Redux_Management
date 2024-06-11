0d0a643eef3a1cee27370da94b4e978f683ecbce
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2024-06-11 11:28:03 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,10 @@
 // file: store.ts
+// file: store.ts
+import { configureStore } from "@reduxjs/toolkit";
+import { reducer } from "./reducer";
+
+const store = configureStore({
+  reducer,
+});
+
+export default store;
